48170ec96ca38e9af38bd4a1ae4816cdc1d32b1c
Viacheslav Sergeev <adviservs@gmail.com>
2023-05-17 20:44:24 +1000

    Lesson 11. Validation errors, user messages, and error displays

--- app/Http/Requests/Worker/StoreRequest.php
+++ app/Http/Requests/Worker/StoreRequest.php
@@ -30,4 +30,19 @@ class StoreRequest extends FormRequest
             'is_married' => 'nullable|string',
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Поле "Имя" является обязательным.',
+            'name.string' => 'Поле "Имя" должно быть строкой.',
+            'surname.required' => 'Поле "Фамилии" является обязательным.',
+            'surname.string' => 'Поле "Фамилии" должно быть строкой.',
+            'email.required' => 'Поле "email" является обязательным.',
+            'email.string' => 'Поле должно быть электронным адресом.',
+            'age.string' => 'Поле должно быть числом.',
+            'description.string' => 'Поле должно быть строкой.',
+            'is_married.string' => 'Поле должно быть строкой.',
+        ];
+    }
 }
--- resources/views/worker/create.blade.php
+++ resources/views/worker/create.blade.php
@@ -11,22 +11,49 @@
 Create page
 <div>
     <hr>
-        <div>
-            <form action="{{ route('worker.store') }}" method="Post">
-                @csrf
-                <div style="margin-bottom: 15px;"><input type="text" name="name" placeholder="name"></div>
-                <div style="margin-bottom: 15px;"><input type="text" name="surname" placeholder="surname"></div>
-                <div style="margin-bottom: 15px;"><input type="email" name="email" placeholder="email"></div>
-                <div style="margin-bottom: 15px;"><input type="number" name="age" placeholder="age"></div>
-                <div style="margin-bottom: 15px;"><textarea name="description" placeholder="description"></textarea></div>
-                <div style="margin-bottom: 15px;">
-                    <input id="isMarried" type="checkbox" name="is_married">
-                    <label for="isMarried">Is married</label>
-                </div>
-                <div style="margin-bottom: 15px"><input type="submit" value="Добавить"></div>
-            </form>
-        </div>
-        <hr>
+    <div>
+        <form action="{{ route('worker.store') }}" method="Post">
+            @csrf
+            <div style="margin-bottom: 15px;">
+                <input type="text" name="name" placeholder="name" value="{{ old('name') }}">
+                @error('name')
+                {{ $message }}
+                @enderror
+            </div>
+            <div style="margin-bottom: 15px;"><input type="text" name="surname" placeholder="surname" value="{{ old('surname') }}">
+                @error('surname')
+                {{ $message }}
+                @enderror
+            </div>
+            <div style="margin-bottom: 15px;"><input type="email" name="email" placeholder="email" value="{{ old('email') }}">
+                @error('email')
+                {{ $message }}
+                @enderror
+            </div>
+            <div style="margin-bottom: 15px;"><input type="number" name="age" placeholder="age" value="{{ old('age') }}">
+                @error('age')
+                {{ $message }}
+                @enderror
+            </div>
+            <div style="margin-bottom: 15px;">
+                <textarea name="description" placeholder="description">
+                    {{ old('description') }}
+                </textarea>
+                @error('description')
+                {{ $message }}
+                @enderror
+            </div>
+            <div style="margin-bottom: 15px;">
+                <input {{ old('is_married') == 'on' ? 'checked' : ''}} id="isMarried" type="checkbox" name="is_married">
+                <label for="isMarried">Is married</label>
+                @error('is_married')
+                {{ $message }}
+                @enderror
+            </div>
+            <div style="margin-bottom: 15px"><input type="submit" value="Добавить"></div>
+        </form>
+    </div>
+    <hr>
 </div>
 </body>
 </html>
