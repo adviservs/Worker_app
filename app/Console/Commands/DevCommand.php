692a80180cbd862f91cb7fed5156fb534ded3723
Viacheslav Sergeev <adviservs@gmail.com>
2023-05-25 17:57:23 +1000

    Lesson 17. Console command

---
+++ app/Console/Commands/DevCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DevCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'develop';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Some develops';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        dd(1234567890);
+        return 0;
+    }
+}
